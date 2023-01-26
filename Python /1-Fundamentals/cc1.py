67a946ddffb6d87c4632618d4eba8b609b2d3e5f
chiche <chides2012@gmail.com>
2023-01-26 06:36:06 +0100

    challenge one completed

---
+++ Python /1-Fundamentals/cc1.py
@@ -0,0 +1,18 @@
+#Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a))
+
+b = 3.5
+print(type(b))
+
+c = "I love python"
+print(type(c))
+
+d = a + b
+print(d)
+
+
+
---
+++ Python /1-Fundamentals/math_operators.py
@@ -0,0 +1,59 @@
+"""
+Addition 
+"""
+x = 1 
+y  = 2
+z = 3
+xyz = x + y + z 
+print( xyz )
+x = x + 10
+print(x)
+y = y + 7
+print(y)
+z = z + 9 
+print(z)
+
+"""
+Substraction  
+"""
+x = 10 
+y =  4
+
+z = x-y 
+print (z)
+
+#multiplication 
+x = 3
+y = 12 
+
+z = x * y 
+print(z)
+
+# Division 
+x = 15
+y = 3
+
+z = x/y
+
+#floor division  only display the whole number with any decimal point 
+
+x = 20
+y = 5
+z = x // y 
+print(z)
+
+#exponentiation 
+
+x = 2 ** 0
+
+#module 
+x - 23 
+y = 5 
+z = x // y
+
+#modulo display only the differnce 
+
+x = 5
+y = 5
+
+z = x % y
