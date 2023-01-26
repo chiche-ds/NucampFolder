5a57da98fa9fdde85621e5c016b5df21e8c93d1b
chiche <chides2012@gmail.com>
2023-01-26 23:56:57 +0100

    chanllenge two

---
+++ Python /1-Fundamentals/cc_if (1).py
@@ -0,0 +1,15 @@
+""""
+  Students  |  Grades  |  Letters
+------------|----------|----------
+  George    |  46      |  F
+  Michell   |  80      |  B
+  Josh      |  12      |  F
+  Chloe     |  68      |  D
+  Stanley   |  99      |  A
+  Annie     |  100     |  A+
+"""
+gradeToTest = 0
+if gradeToTest == 100:
+    print("Passing")
+else:
+    print("Failing")
