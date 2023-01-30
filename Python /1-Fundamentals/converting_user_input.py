502cca9ff632cdd43cf7711a2a9782891003bcce
chiche <chides2012@gmail.com>
2023-01-30 16:55:29 +0100

    custom finction

---
+++ Python /1-Fundamentals/converting_user_input.py
@@ -0,0 +1,4 @@
+total_cash = 0
+user_input = input("Enter amount transfer")
+total_cash = total_cash + float(user_input)
+print(total_cash)
---
+++ Python /1-Fundamentals/custom_functions.py
@@ -0,0 +1,11 @@
+def myfun():
+    print("you have called a function")
+
+
+def add(x,y):
+    z = x + y 
+    print(z)
+
+myfun()
+add(2,3)
+add(3,5)
