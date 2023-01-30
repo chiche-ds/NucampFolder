0cb4131bdee21628b5470fb7cca4867614209031
chiche <chides2012@gmail.com>
2023-01-30 06:41:35 +0100

    forloop with range function

---
+++ Python /1-Fundamentals/forloop_range.py
@@ -0,0 +1,14 @@
+for x in range(0, 10, 1):
+    print(x)
+print("For loop has finish")
+#changing the stop value 
+print("let's change the step value")
+for x in range(0, 10, 2):
+    print(x)
+#let change the start value
+print("changing the start value")
+for x in range(-5, 10, 2):
+    print(x)
+#negative values 
+for x in range(-5, -10, -1):
+    print(x)
