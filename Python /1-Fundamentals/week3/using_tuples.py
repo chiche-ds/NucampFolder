897559e284e32f9d15bf4f735c07ca3e9abd93e9
chiche <chides2012@gmail.com>
2023-02-14 05:18:26 +0100

    tuple

---
+++ Python /1-Fundamentals/week3/using_tuples.py
@@ -0,0 +1,11 @@
+my_tuple = (1,2,3,4,5,6,)
+#access turple values 
+#print(my_tuple[0] + my_tuple[1])
+
+print(my_tuple[-1] + my_tuple[-2])
+
+#my_tuple[3] = 1
+
+my_tuple = (5,4,3,2,1)
+print(my_tuple)
+
