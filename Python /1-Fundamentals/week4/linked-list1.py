98b00253bb0584215e6c79f5a5f765f0d59993ca
chiche <chides2012@gmail.com>
2023-02-21 23:56:56 +0100

    travease a link list

---
+++ Python /1-Fundamentals/week4/linked-list1.py
@@ -0,0 +1,25 @@
+""" 
+ """
+
+
+class Player:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp 
+        self.score = 0
+
+
+
+
+player1 = Player("Aaron", 1200)
+player2  = Player("Irene", 1300)
+
+
+print("P1:", player1.name, " -- HP: ",player1.hp, " -- SCORE: ", player1.score)
+print("P1:", player2.name, " -- HP: ",player2.hp, " -- SCORE: ", player2.score)
+
+player1.hp += 500
+player1.score += 10
+
+print("P1:", player1.name, " -- HP: ",player1.hp, " -- SCORE: ", player1.score)
+print("P1:", player2.name, " -- HP: ",player2.hp, " -- SCORE: ", player2.score)
