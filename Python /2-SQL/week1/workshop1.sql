bd496274355c165eb5f7623c58f9551dda02e5b2
chiche <chides2012@gmail.com>
2023-03-11 20:33:53 +0100

    learning postgress

---
+++ Python /2-SQL/week1/workshop1.sql
@@ -0,0 +1,125 @@
+-- kill other connections
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'week1_workshop' AND pid <> pg_backend_pid();
+-- (re)create the database
+DROP DATABASE IF EXISTS week1_workshop;
+CREATE DATABASE week1_workshop;
+-- connect via psql
+\c week1_workshop
+
+-- database configuration
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET default_tablespace = '';
+SET default_with_oids = false;
+
+
+---
+--- CREATE tables
+---
+
+CREATE TABLE products (
+    id SERIAL,
+    name TEXT NOT NULL,
+    discontinued BOOLEAN NOT NULL,
+    supplier_id INT,
+    category_id INT,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE categories (
+    id SERIAL,
+    name TEXT UNIQUE NOT NULL,
+    description TEXT,
+    picture TEXT,
+    PRIMARY KEY (id)
+);
+
+-- TODO create more tables here...
+CREATE TABLE suppliers (
+    id SERIAL,
+    name TEXT  NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE customers (
+    id SERIAL,
+    company_name TEXT  NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+    id SERIAL,
+    first_name TEXT  NOT NULL,
+    last_name TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE orders (
+    id SERIAL,
+    date date,
+    customer_id INT,
+    employee_id INT,
+    PRIMARY KEY (id)
+);
+CREATE TABLE orders_products (
+    product_id INT,
+    order_id INT,
+    quantity INT,
+    discount NUMERIC,
+    PRIMARY KEY (product_id)
+);
+CREATE TABLE territories (
+    id SERIAL,
+    description TEXT,
+    PRIMARY KEY (id)
+);
+CREATE TABLE employees_territories (
+    employee_id INT,
+    territory_id INT,
+    PRIMARY KEY (employee_id ,territory_id)
+);
+CREATE TABLE offices (
+    id SERIAL,
+    address_line TEXT NOT NULL,
+    territory_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE us_states (
+    id SERIAL,
+    name TEXT NOT NULL,
+    abbreviation CHAR(2) NOT NULL,
+    PRIMARY KEY(id)
+);
+---
+--- Add foreign key constraints
+---
+
+-- PRODUCTS
+
+ALTER TABLE products
+ADD CONSTRAINT fk_products_categories 
+FOREIGN KEY (category_id) 
+REFERENCES categories (id);
+
+
+-- TODO create more constraints here...
+ALTER TABLE orders 
+ADD CONSTRAINT fk_orders_customers
+FOREIGN KEY (customer_id) 
+REFERENCES customers (id);
+
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_employees
+FOREIGN KEY (employee_id)
+REFERENCES employees (id);
+
+ALTER TABLE products
+ADD CONSTRAINT fk_products_suppliers 
+FOREIGN KEY (supplier_id) 
+REFERENCES suppliers (id);
