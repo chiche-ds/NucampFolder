4d9fad7b59f13405cd23c880d9b8f7e100009167
chiche <chides2012@gmail.com>
2023-03-23 20:42:15 +0100

    flask execise

--- Python /2-SQL/week3/sqlalchemy/veggies.py
+++ Python /2-SQL/week3/sqlalchemy/veggies.py
@@ -3,6 +3,56 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 #connect to postgres database 
 
-engine = create_engine('postgresql://postgress@localhost:5432/week3')
+engine = create_engine('postgresql://postgres@localhost:5432/week3')
 Session = sessionmaker(bind=engine)
-base = declarative_base()
+Base = declarative_base()
+
+class Veggie(Base):
+    __tablename__ = "veggies"
+
+    # set autoincrement to use the SERIAL data type
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    color = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+
+    def formatted_name(self):
+        return self.color.capitalize() + " " + self.name.capitalize()
+
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
+
+seed_data = [
+    {'name': 'carrot', 'color': 'orange'},
+    {'name': 'onion', 'color': 'yellow'},
+    {'name': 'zucchini', 'color': 'green'},
+    {'name': 'squash', 'color': 'yellow'},
+    {'name': 'pepper', 'color': 'red'},
+    {'name': 'onion', 'color': 'red'}
+]
+
+# Turn the seed data into a list of Veggie objects
+veggie_objects = []
+for item in seed_data:
+    v = Veggie(name=item["name"], color=item["color"])
+    veggie_objects.append(v)
+
+# Create a session, insert new records, and commit the session
+session = Session()
+session.bulk_save_objects(veggie_objects)
+session.commit()    
+
+#create a new session for performing queries 
+session = Session()
+
+#Run a SELECT * query on the veggies table 
+veggies = session.query(Veggie).all()
+
+for v in veggies:
+    print(v.color, v.name)
+
+veggies = session.query(Veggie).order_by(Veggie.name, Veggie.color).all()
+
+for i, v in enumerate(veggies):
+    print(str(i+1) + "."+ v.formatted_name())
+
+
